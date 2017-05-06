781b47fab7f2fde57e3790505c180c65e56769b9
vamsinath reddy <vamsi2chakri@gmail.com>
2017-05-07 00:00:00 +0530

    Changes are made in controller class.

--- src/main/java/com/ismav/controller/RoughController.java
+++ src/main/java/com/ismav/controller/RoughController.java
@@ -2,15 +2,22 @@ package com.ismav.controller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-@Path("/hello")
+@Path("/user")
 public class RoughController {
 
 	@GET
+	@Path("/{code}")
 	@Produces("text/plain")
-	public String getName(){
-		return "vamsinath";
+	public String getName(@PathParam("code") int number){
+		if(number==007){
+		return "vamsi";
+		}else{
+			return number+"";
+		}
+		
 	}
 	
 }
